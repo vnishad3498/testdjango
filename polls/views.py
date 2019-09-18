418d4cf58833e4ea77416a7312e9565687cb5714
Virendra <virendranishad3498@gmail.com>
2019-09-18 08:48:52 +0530

    Adding Mysitr Project

---
+++ polls/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'home.html', {'name':'Virendra'})
+
+
+def add(request):
+    val1 = int(request.POST["num1"])
+    val2 = int(request.POST["num2"])
+    res = val1 + val2
+    return render(request, 'result.html', {'result': res})
